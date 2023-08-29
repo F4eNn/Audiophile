75cbe0011d7df1e4167835f43f57c082a89bf1f0
Mateusz <mateusz4k@outlook.com>
2023-08-29 17:40:27 +0200

    feat: add initial config prettier/tailwind/esling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,92 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			primary: '#D87D4A',
+			secondary: '#FBAF85',
+			primaryDark: '#000000',
+			secondaryDark: '#101010',
+			white: '#FFFFFF',
+			lightWhite: '#FAFAFA',
+			rose: '#F1F1F1',
+		},
+		extend: {
+			fontSize: {
+				H1: [
+					'3.1rem',
+					{
+						lineHeight: '3.2rem',
+						letterSpacing: '2px',
+						fontWeight: '700',
+					},
+				],
+				H2: [
+					'3.2rem',
+					{
+						lineHeight: '2.4rem',
+						letterSpacing: '1.5px',
+						fontWeight: '700',
+					},
+				],
+				H3: [
+					'1.75rem',
+					{
+						lineHeight: '2em',
+						letterSpacing: '1.15px',
+						fontWeight: '700',
+					},
+				],
+				H4: [
+					'1.5rem',
+					{
+						lineHeight: '2.1rem',
+						letterSpacing: '2px',
+						fontWeight: '700',
+					},
+				],
+				H5: [
+					'1.3rem',
+					{
+						lineHeight: '1.8rem',
+						letterSpacing: '1.7px',
+						fontWeight: '700',
+					},
+				],
+				H6: [
+					'1rem',
+					{
+						lineHeight: '1.3',
+						letterSpacing: '1.3px',
+						fontWeight: '700',
+					},
+				],
+				overline: [
+					'0.75',
+					{
+						lineHeight: '1.05',
+						letterSpacing: '10px',
+						fontWeight: '400',
+					},
+				],
+				subtitle: [
+					'0.7',
+					{
+						lineHeight: '1.4',
+						letterSpacing: '1px',
+						fontWeight: '700',
+					},
+				],
+			},
+		},
+	},
+	plugins: [
+    require('@tailwindcss/forms')
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
 }
 export default config
