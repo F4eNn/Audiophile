898274afffd01c600027bd86c6a20f0282bab4fe
Mateusz <mateusz4k@outlook.com>
2023-10-12 12:34:09 +0200

    fix: issue that in userProfile acc creation time start from -1 in several cases

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,48 +1,53 @@
-import Cookies from 'js-cookie';
+import Cookies from 'js-cookie'
 
-import { STRAPI_URL } from '@/constants/url';
+import { STRAPI_URL } from '@/constants/url'
 
 export type DataTokenResponse = {
 	user: {
-		username: string;
-		id: string;
-	};
-	jwt: string;
-};
+		username: string
+		id: string
+	}
+	jwt: string
+}
 
 export const setToken = (data: DataTokenResponse) => {
-	if (typeof window === 'undefined') return;
-	Cookies.set('id', data.user.id);
-	Cookies.set('username', data.user.username);
-	Cookies.set('jwt', data.jwt);
-};
+	if (typeof window === 'undefined') return
+	Cookies.set('id', data.user.id)
+	Cookies.set('username', data.user.username)
+	Cookies.set('jwt', data.jwt)
+}
 
 export const unsetToken = () => {
-	if (typeof window === 'undefined') return;
+	if (typeof window === 'undefined') return
 
-	Cookies.remove('id');
-	Cookies.remove('username');
-	Cookies.remove('jwt');
-};
+	Cookies.remove('id')
+	Cookies.remove('username')
+	Cookies.remove('jwt')
+}
 
 export const getUserFromLocalCookie = async () => {
-	const jwt = getTokenFromLocalCookie();
-	const data = await getUser(jwt);
-	return data && data.username;
-};
+	const jwt = getTokenFromLocalCookie()
+	const data = await getUser(jwt)
+	return data && data.username
+}
 const getUser = async (jwt: string | undefined) => {
 	if (jwt) {
-		const res = await fetch(`${STRAPI_URL}/users/me`, {
-			headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${jwt}` },
-		});
-		const resData = await res.json();
-		if (!resData) {
-			return console.error(`User fetch failed: ${resData.error.message} `);
+		try {
+			const res = await fetch(`${STRAPI_URL}/users/me`, {
+				headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${jwt}` },
+			})
+
+			if (!res.ok) {
+				throw new Error('Network response was not ok')
+			}
+			const resData = await res.json()
+			return resData
+		} catch (error) {
+			console.error('Error fetching user data:', error)
 		}
-		return resData;
 	}
-};
+}
 
 export const getTokenFromLocalCookie = () => {
-	return Cookies.get('jwt');
-};
+	return Cookies.get('jwt')
+}
