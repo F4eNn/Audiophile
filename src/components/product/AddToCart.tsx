4fad427821879bf07bed1912217fa4644381d595
Mateusz <mateusz4k@outlook.com>
2023-10-13 12:34:45 +0200

    fix: issue that input not stretch at values greater than 100

--- src/components/product/AddToCart.tsx
+++ src/components/product/AddToCart.tsx
@@ -1,45 +1,45 @@
-'use client';
-import React, { useState, useRef, FormEvent } from 'react';
+'use client'
+import React, { useState, useRef, FormEvent } from 'react'
 
-import { CartTypes, useCartCtx } from '@/context/CartCtx';
-import { QuantityInput } from '../ui/QuantityInput';
+import { CartTypes, useCartCtx } from '@/context/CartCtx'
+import { QuantityInput } from '../ui/QuantityInput'
 
 export const AddToCart = ({ image, price, name }: Partial<CartTypes>) => {
-	const [delay, setDelay] = useState(true);
-	const quantityRef = useRef<HTMLInputElement>(null);
+	const [delay, setDelay] = useState(true)
+	const quantityRef = useRef<HTMLSpanElement>(null)
 
-	const { addToCart: addProductToCart, setAddCart } = useCartCtx();
+	const { addToCart: addProductToCart, setAddCart } = useCartCtx()
 
 	const createShorterName = () => {
-		const splitName = name!.split(' ');
-		const removeDeviceName = ['Headphones', 'Speaker', 'Earphones', 'Wireless'];
-		const shorterName = splitName.filter(p => !removeDeviceName.includes(p));
+		const splitName = name!.split(' ')
+		const removeDeviceName = ['Headphones', 'Speaker', 'Earphones', 'Wireless']
+		const shorterName = splitName.filter(p => !removeDeviceName.includes(p))
 
-		return shorterName.join(' ');
-	};
+		return shorterName.join(' ')
+	}
 
 	const addToCart = (e: FormEvent) => {
-		e.preventDefault();
-		const shorterName = createShorterName();
-		const quantity = parseFloat(quantityRef.current!.value);
+		e.preventDefault()
+		const shorterName = createShorterName()
+		const quantity = parseFloat(quantityRef.current!.textContent!)
 
 		if (delay) {
-			setDelay(false);
-			setAddCart(true);
+			setDelay(false)
+			setAddCart(true)
 			setTimeout(() => {
-				addProductToCart({ image: image!, name: shorterName, price: price!, quantity });
-				setDelay(true);
-				setAddCart(false);
-			}, 300);
+				addProductToCart({ image: image!, name: shorterName, price: price!, quantity })
+				setDelay(true)
+				setAddCart(false)
+			}, 300)
 		}
-	};
+	}
 
 	return (
 		<form className='flex gap-4' onSubmit={addToCart}>
-			<QuantityInput ref={quantityRef} />
+			<QuantityInput ref={quantityRef} pInput='px-5' />
 			<button className='colors-300 min-w-max max-w-[175px] flex-1 bg-primary px-6 uppercase text-white  hover:bg-secondary'>
 				add to cart
 			</button>
 		</form>
-	);
-};
+	)
+}
--- src/components/ui/QuantityInput.tsx
+++ src/components/ui/QuantityInput.tsx
@@ -1,63 +1,59 @@
-'use client';
-import React, { useState, forwardRef, ForwardedRef, useEffect } from 'react';
+'use client'
+import React, { useState, forwardRef, ForwardedRef, useEffect } from 'react'
 
-import { useCartCtx } from '@/context/CartCtx';
+import { useCartCtx } from '@/context/CartCtx'
 
 type QuantityInputProps = {
-	productQuantity?: number;
-	py?: string;
-	px?: string;
-	pInput?: string;
-	idx?: number;
-	isChangeQuantity?: boolean;
-};
+	productQuantity?: number
+	py?: string
+	px?: string
+	pInput?: string
+	idx?: number
+	isChangeQuantity?: boolean
+}
 
 export const QuantityInput = forwardRef(
 	(
 		{ productQuantity = 1, px = 'px-3', py = 'py-2.5', pInput, idx, isChangeQuantity = false }: QuantityInputProps,
-		ref: ForwardedRef<HTMLInputElement>,
+		ref: ForwardedRef<HTMLSpanElement>,
 	) => {
-		const { setDecrementQuantity, setIncremenetQuantity } = useCartCtx();
+		const { setDecrementQuantity, setIncremenetQuantity } = useCartCtx()
 
-		const [quantity, setQuantity] = useState(productQuantity);
+		const [quantity, setQuantity] = useState(productQuantity)
 
 		const incrementQuantity = () => {
-			setQuantity(prev => prev + 1);
+			setQuantity(prev => prev + 1)
 			if (isChangeQuantity) {
-				setIncremenetQuantity({ idx: idx!, isChange: true });
+				setIncremenetQuantity({ idx: idx!, isChange: true })
 			}
-		};
+		}
 		const decrementQuantity = () => {
-			setQuantity(prev => prev - 1);
+			setQuantity(prev => prev - 1)
 			if (isChangeQuantity) {
-				setDecrementQuantity({ idx: idx!, isChange: true });
+				setDecrementQuantity({ idx: idx!, isChange: true })
 			}
-		};
+		}
 
 		useEffect(() => {
-			setQuantity(productQuantity);
-		}, [productQuantity]);
+			setQuantity(productQuantity)
+		}, [productQuantity])
 
 		return (
-			<div className='w-max bg-rose'>
+			<div className='flex items-center justify-center bg-rose'>
 				<button
 					onClick={decrementQuantity}
 					type='button'
-					className={`colors-300 ${py}  ${px} text-lg text-brown hover:text-primary disabled:cursor-not-allowed disabled:hover:text-brown`}
+					className={`colors-300  ${py}  ${px} text-lg text-brown hover:text-primary disabled:cursor-not-allowed disabled:hover:text-brown`}
 					disabled={quantity <= 1}
 				>
 					-
 				</button>
-				<input
+				<span
 					ref={ref}
-					type='number'
-					className={`input pointer-events-none border-none bg-rose ${pInput}   text-center  font-bold focus:outline-0 focus:ring-0`}
-					value={quantity}
-					min={1}
-					tabIndex={-1}
-					max={99}
-					readOnly
-				/>
+					className={`input pointer-events-none border-none bg-rose text-center ${pInput} font-bold focus:outline-0 focus:ring-0`}
+				>
+					{quantity}
+				</span>
 				<button
 					onClick={incrementQuantity}
 					type='button'
@@ -67,8 +63,8 @@ export const QuantityInput = forwardRef(
 					+
 				</button>
 			</div>
-		);
+		)
 	},
-);
+)
 
-QuantityInput.displayName = 'Quantity Input';
+QuantityInput.displayName = 'Quantity Input'
